0cabf6811c669a12fefbad935136c1720a24643b
Jazmin Villagra <jazmin.villagra@cycleon.com>
2023-01-17 17:22:14 +0100

    Add update posts route

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use((req, res, next) =>{
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, PATCH, POST, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS')
     next();
 });
 
@@ -60,4 +60,25 @@ app.delete('/api/posts/:id', (req, res, next) => {
     });
 });
 
+app.put("/api/posts/:id", (req, res, next) => {
+    const post = new Post({
+        _id: req.params.id,
+        title: req.body.title,
+        content: req.body.content
+    })
+    Post.updateOne({_id: req.params.id}, post).then(result => {
+        console.log(result);
+        res.status(200).json({message: 'Post updated!'});
+    })
+})
+
+app.get("/api/posts/:id", (req, res, next) => {
+    Post.findById(req.params.id).then(post => {
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({message: 'Post not found'})
+        }
+    });
+})
 module.exports = app;
